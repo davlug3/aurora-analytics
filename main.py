7f59807472f7ef3c3d4aa413c42903db9aef95a4
Dave <davelugtu1@gmail.com>
2026-02-26 13:09:27 +0800

    feat: added kinesis

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import requests
 import time
 from pydantic import BaseModel, Field, ValidationError
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any
+import boto3
 
 NEWSAPI_KEY = os.getenv("NEWSAPI_KEY")
 NEWSAPI_URL = os.getenv("NEWSAPI_BASE", "https://newsapi.org/v2/everything")
 API_FETCH_INTERVAL = int(os.getenv("API_FETCH_INTERVAL", 120))
+
+KINESIS_STREAM_NAME = os.getenv("KINESIS_STREAM_NAME", "news-stream")
+AWS_REGION = os.getenv("AWS_REGION", "ap-southeast-1")
+
+kinesis_client = boto3.client("kinesis", region_name=AWS_REGION)
+session = requests.Session()
 
 
 class Article(BaseModel): 
@@ -19,7 +26,7 @@ class Article(BaseModel):
     Url: str | None = None
     author: str | None = None
     published_at: datetime | None = None
-    ingested_at: datetime = Field(default_factory=lambda: datetime.now())
+    ingested_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 def validate_article(article: dict[str, Any]) ->  Article | None:
     try:
@@ -39,14 +46,19 @@ def validate_article(article: dict[str, Any]) ->  Article | None:
 
 
 def send_to_kinesis(article: Article):
-    with open('log.txt', 'a') as f:
-        f.write(article.model_dump_json())
-        f.write('\n\n')
+    try:
+        data = article.model_dump_json()
+        kinesis_client.put_record(
+            StreamName=KINESIS_STREAM_NAME,
+            Data=data,
+            PartitionKey=article.article_id,
+        )
+    except Exception as err:
+        print(f"Failed to write to AWS Kinesis: {err}")
 
     
 def fetch_news(q = "technology", page_size=20) -> list[Article]:
-    print(f"Fetching news for query: {q}")
-    session = requests.Session()
+    print(f"[{datetime.now(timezone.utc)}] Fetching news for query: {q}")
     response = session.get(NEWSAPI_URL, params={
         "q": q,
         "apiKey": NEWSAPI_KEY,
@@ -65,16 +77,23 @@ def fetch_news(q = "technology", page_size=20) -> list[Article]:
 
 
 def main(): 
+    if not NEWSAPI_KEY: 
+        print("NEWSAPI_KEY is not set")
+        return
+
+
+
     while True:
         try: 
             articles = fetch_news()
             if articles:
                 for article in articles: 
                     send_to_kinesis(article)
-            print(f"Processed {len(articles)} articles")
+            print(f"Successfully processed {len(articles)} articles")
             time.sleep(API_FETCH_INTERVAL)
         except Exception as err: 
             print(f"Error: {err}")
+            time.sleep(10)
 
 
 if __name__ == "__main__": 
